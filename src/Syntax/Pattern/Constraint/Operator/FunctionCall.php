cca6d442d125a8be81033695658761df1f1c9c31
Roensby <pr@activeforanimals>
2026-03-27 15:47:55 +0100

    Fix FunctionCall

--- src/Syntax/Pattern/Constraint/Operator/FunctionCall.php
+++ src/Syntax/Pattern/Constraint/Operator/FunctionCall.php
@@ -2,18 +2,15 @@
 
 namespace EffectiveActivism\SparQlClient\Syntax\Pattern\Constraint\Operator;
 
-use EffectiveActivism\SparQlClient\Exception\SparQlException;
+use EffectiveActivism\SparQlClient\Syntax\Pattern\Constraint\Operator\Variadic\VariadicOperatorInterface;
 use EffectiveActivism\SparQlClient\Syntax\Term\Iri\AbstractIri;
 use EffectiveActivism\SparQlClient\Syntax\Term\TermInterface;
 
-class FunctionCall implements OperatorInterface
+class FunctionCall implements VariadicOperatorInterface
 {
     /** @var array<OperatorInterface|TermInterface> */
     protected array $arguments;
 
-    /**
-     * @throws SparQlException
-     */
     public function __construct(protected AbstractIri $functionIri, OperatorInterface|TermInterface ...$arguments)
     {
         $this->arguments = $arguments;
@@ -23,6 +20,11 @@ class FunctionCall implements OperatorInterface
     {
         $parts = array_map(fn(OperatorInterface|TermInterface $arg) => $arg->serialize(), $this->arguments);
         return sprintf('%s(%s)', $this->functionIri->serialize(), implode(', ', $parts));
+    }
+
+    public function getExpressions(): array
+    {
+        return array_merge([$this->functionIri], $this->arguments);
     }
 
     public function getFunctionIri(): AbstractIri
